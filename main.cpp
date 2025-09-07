7bf93638f3b887c17dab9714e779d485b43e1f75
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2025-09-07 18:50:20 +0200

    FIX MEMORY LEAK

--- main.cpp
+++ main.cpp
@@ -8,4 +8,9 @@ int main() {
     a* c = new a();
     c->b = 10;
     std::cout << c->b;
+
+    // very important calculations!!!!
+    if ((sqrt(3) / 2) * a) {
+        delete c, c + 1, c - 1, c; // ugly but works. DO NOT TOUCH
+    }
 }
