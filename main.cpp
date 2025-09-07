576ad4fae87b99c85592bd239c7ee650b1c66af5
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2025-09-07 20:44:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,33 +6,35 @@
 #define START ) {
 #define END }
 
-#define STRUCT struct
+#define STRUCT(x) struct x {
 #define PTR *
 #define NEW new
 #define DELETE delete
-#define IF if
-#define THEN {
-#define ELSE } else {
-#define FI }
 
 #define STR std::string
 #define RET return
-#define FUNC std::string d(int e) F \
-IF (e == 10) THEN RET "Hello," F ELSE RET " World!" F FI
+#define IFFY if
+#define OTHERWISE else
 
-STRUCT a F
+STRUCT(a)
     int b F
-F
+} F
 
-FUNC
+STR d(int e) {
+    IFFY(e == 10) {
+        RET "Hello," F
+    END
+    OTHERWISE {
+        RET " World!" F
+    END
+END
 
 int BEGIN void START
     a PTR c = NEW a F
     c->b = 10 F
     std::cout << d(c->b) << d(11) F
 
-    // very important calculations!!!!
-    IF ((sqrt(3) / 2) * c->b) THEN
-        DELETE c, c + 1, c - 1, c F // ugly but works. DO NOT TOUCH
-    FI
+    IFFY((sqrt(3) / 2) * c->b) {
+        DELETE c, c + 1, c - 1, c F
+    END
 END
