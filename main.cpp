892b7f25539ddc1fd9e45298bd4f6e23f4bf7a67
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2025-09-07 20:38:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <cmath>
 
-struct a {
-    int b;
-};
+#define F ;
+#define BEGIN main(
+#define START ) {
+#define END }
 
-std::string d(int e) {if (e == 10) return "Hello,"; return " World!";}
+#define STRUCT struct
+#define PTR *
+#define NEW new
+#define DELETE delete
+#define IF if
+#define THEN {
+#define ELSE } else {
+#define FI }
 
-int main() {
-    a* c = new a();
-    c->b = 10;
-    std::cout << d(c->b) << d(11);
+#define STR std::string
+#define RET return
+#define FUNC std::string d(int e) F \
+IF (e == 10) THEN RET "Hello," F ELSE RET " World!" F FI
+
+STRUCT a F
+    int b F
+F
+
+FUNC
+
+int BEGIN void START
+    a PTR c = NEW a F
+    c->b = 10 F
+    std::cout << d(c->b) << d(11) F
 
     // very important calculations!!!!
-    if ((sqrt(3) / 2) * a) {
-        delete c, c + 1, c - 1, c; // ugly but works. DO NOT TOUCH (i will touch it where the sun don't shine) (no you won't)
-    }
-}
+    IF ((sqrt(3) / 2) * c->b) THEN
+        DELETE c, c + 1, c - 1, c F // ugly but works. DO NOT TOUCH
+    FI
+END
